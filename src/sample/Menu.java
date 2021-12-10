54b76954c8cca88b480b84563b5e4ba67f6521de
AzizaSy <sadykovaziza511@gmail.com>
2021-12-10 12:56:39 +0600

    Initial commit

---
+++ src/sample/Menu.java
@@ -0,0 +1,81 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Menu  {
+
+    @FXML
+    private Button go;
+
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private Label loginMassage;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    void Scene2(ActionEvent event) throws IOException {
+
+        Stage s = (Stage) go.getScene().getWindow();
+        s.close();
+
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/Scene2.fxml"));
+        Scene scene = new Scene((Parent) fxmlLoader.load(),640,375);
+        stage.setTitle("StroyShop");
+        stage.setScene(scene);
+        stage.show();
+    }}
+
+
+
+
+
+
+
+
+
+//public void goButtonOnAction(ActionEvent event){
+//    if(login.getText().isBlank() == false && password.getText().isBlank() == false){
+//        valiadatelogin();
+//    }else{
+//        loginMassage.setText("Please enter your login and password");
+//
+//    }
+
+
+
+//public void loginOnAction(){
+//
+//
+//}
+//
+//
+//public void cancelOnAction(ActionEvent event){
+//        Stage stage = (Stage) cancel.getScene().getWindow();
+//        stage.close();
+//    }
+//
+//    public void valiadatelogin(){
+//
+//    }
+//}
+//
