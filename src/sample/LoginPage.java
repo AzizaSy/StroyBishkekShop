05ce2304cba64684412c71bdf3d70b484137008d
Glebii <60538108+Glebii@users.noreply.github.com>
2021-12-13 12:37:11 +0600

    Gleb LoginPage

---
+++ src/sample/LoginPage.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginPage {
+
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private Button go;
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private Label loginMassage;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    void Cancelation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotoCMORAM(ActionEvent event) {
+
+    }
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
 
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginPage.fxml")));
         primaryStage.setTitle("Bishkek Shop");
         primaryStage.setScene(new Scene(root, 640, 370));
         primaryStage.show();
